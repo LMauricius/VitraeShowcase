830df8bd242d30790ca30b3680a3d7106ec7cad2
LMauricius <mauriciosmit100@gmail.com>
2024-08-20 17:40:26 +0200

    New ebr method

--- Showcase/src/Methods/shadowEBR.hpp
+++ Showcase/src/Methods/shadowEBR.hpp
@@ -31,14 +31,75 @@ struct MethodsShadowEBR : MethodCollection
         FRAGMENT SHADING
         */
 
+        /*auto p_alias =
+            root.getComponent<ShaderFunctionKeeper>().new_asset({ShaderFunction::StringParams{
+                .inputSpecs =
+                    {
+                        PropertySpec{.name = "position_shadow_view",
+                                     .typeInfo = StandardShaderPropertyTypes::FRAGMENT_OUTPUT},
+                        PropertySpec{.name = "normal_view",
+                                     .typeInfo = Variant::getTypeInfo<glm::vec3>()},
+                        PropertySpec{.name = "ShadowMapSize",
+                                     .typeInfo = Variant::getTypeInfo<glm::vec2>()},
+                    },
+                .outputSpecs =
+                    {
+                        PropertySpec{.name = "shadow_alias",
+                                     .typeInfo = Variant::getTypeInfo<glm::vec3>()},
+                    },
+                .snippet = R"(
+                    void shadowAlias(
+                        in vec4 position_shadow_view, in vec3 normal_view,
+                        in vec2 ShadowMapSize,
+                        out vec3 alias)
+                    {
+                        vec2 proper_coordinate = (position_shadow_view.xy * 0.5 + 0.5) *
+           ShadowMapSize; vec2 diff = (gl_FragCoord.xy - proper_coordinate); vec2 normal_2d =
+           normalize(normal_view.xy); float normal_based_offset = dot(normal_2d, diff) + 0.5; alias
+           = vec3(normal_2d.xy * sign(normal_based_offset),
+           normal_based_offset*normal_based_offset);
+                    }
+                )",
+                .functionName = "shadowAlias"}});*/
+
+        auto p_viewNormal2D =
+            root.getComponent<ShaderFunctionKeeper>().new_asset({ShaderFunction::StringParams{
+                .inputSpecs =
+                    {
+                        PropertySpec{.name = "position_shadow_view",
+                                     .typeInfo = StandardShaderPropertyTypes::FRAGMENT_OUTPUT},
+                        PropertySpec{.name = "normal_view",
+                                     .typeInfo = Variant::getTypeInfo<glm::vec3>()},
+                        PropertySpec{.name = "ShadowMapSize",
+                                     .typeInfo = Variant::getTypeInfo<glm::vec2>()},
+                    },
+                .outputSpecs =
+                    {
+                        PropertySpec{.name = "shadow_alias",
+                                     .typeInfo = Variant::getTypeInfo<glm::vec3>()},
+                    },
+                .snippet = R"(
+                    void viewNormal2D(
+                        in vec3 normal_view,
+                        out vec2 normal_view2d)
+                    {
+                        normal_view2d = normalize(normal_view.xy);
+                    }
+                )",
+                .functionName = "viewNormal2D"}});
+
         auto p_shadowLightFactor =
             root.getComponent<ShaderFunctionKeeper>().new_asset({ShaderFunction::StringParams{
                 .inputSpecs =
                     {
                         PropertySpec{.name = "tex_shadow",
                                      .typeInfo = Variant::getTypeInfo<dynasma::FirmPtr<Texture>>()},
+                        PropertySpec{.name = "tex_shadow_alias",
+                                     .typeInfo = Variant::getTypeInfo<dynasma::FirmPtr<Texture>>()},
                         PropertySpec{.name = "position_shadow",
                                      .typeInfo = Variant::getTypeInfo<glm::vec3>()},
+                        PropertySpec{.name = "ebr_strength",
+                                     .typeInfo = Variant::getTypeInfo<float>()},
                     },
                 .outputSpecs =
                     {
@@ -56,29 +117,46 @@ struct MethodsShadowEBR : MethodCollection
                     }
 
                     void lightShadowFactor(
-                        in sampler2D tex_shadow, in vec3 position_shadow,
+                        in sampler2D tex_shadow, sampler2D tex_shadow_normal, in vec3 position_shadow,
+                        in float ebr_strength,
                         out float light_shadow_factor)
                     {
                         vec2 shadowSize = textureSize(tex_shadow, 0);
                         float offset = 0.5 / shadowSize.x;
-                        vec2 texelPos = position_shadow.xy * shadowSize;
-                        vec2 topLeftTexelPos = floor(position_shadow.xy * shadowSize);
-                        ivec2 topLeftTexelPosI = ivec2(topLeftTexelPos);
-                        vec2 bilinOffset = texelPos - topLeftTexelPos;
 
-                        light_shadow_factor = (
-                            inShadowTexel(
-                                tex_shadow, topLeftTexelPosI + ivec2(0,0), position_shadow.z, offset
-                            ) * (1.0 - bilinOffset.x) + inShadowTexel(
-                                tex_shadow, topLeftTexelPosI + ivec2(1,0), position_shadow.z, offset
-                            ) * bilinOffset.x
-                        ) * (1.0 - bilinOffset.y) + (
-                            inShadowTexel(
-                                tex_shadow, topLeftTexelPosI + ivec2(0,1), position_shadow.z, offset
-                            ) * (1.0 - bilinOffset.x) + inShadowTexel(
-                                tex_shadow, topLeftTexelPosI + ivec2(1,1), position_shadow.z, offset
-                            ) * bilinOffset.x
-                        ) * bilinOffset.y;
+                        vec2 normal2d = normalize(texture(tex_shadow_normal, position_shadow.xy).xy) / shadowSize;
+                        vec2 tangent2d = vec2(normal2d.y, -normal2d.x);
+
+                        const int numSamples = 4;
+                        const float sampleRange = 2.0;
+
+                        for (int i = 0; i < 4; i++) {
+                            float t = -sampleRange + float(i) / numSamples * 2.0 * sampleRange;
+
+                            vec2 pos_sample = position_shadow.xy + t * tangent2d;
+
+                            vec2 texelPos = pos_sample * shadowSize;
+                            vec2 topLeftTexelPos = floor(pos_sample * shadowSize);
+                            ivec2 topLeftTexelPosI = ivec2(topLeftTexelPos);
+                            vec2 bilinOffset = texelPos - topLeftTexelPos;
+
+                            light_shadow_factor += (
+                                inShadowTexel(
+                                    tex_shadow, topLeftTexelPosI + ivec2(0,0), position_shadow.z, offset
+                                ) * (1.0 - bilinOffset.x) + inShadowTexel(
+                                    tex_shadow, topLeftTexelPosI + ivec2(1,0), position_shadow.z, offset
+                                ) * bilinOffset.x
+                            ) * (1.0 - bilinOffset.y) + (
+                                inShadowTexel(
+                                    tex_shadow, topLeftTexelPosI + ivec2(0,1), position_shadow.z, offset
+                                ) * (1.0 - bilinOffset.x) + inShadowTexel(
+                                    tex_shadow, topLeftTexelPosI + ivec2(1,1), position_shadow.z, offset
+                                ) * bilinOffset.x
+                            ) * bilinOffset.y;
+                        }
+
+                        light_shadow_factor /= numSamples;
+
                         
                         if (light_shadow_factor >= 0.5) {
                             light_shadow_factor = 1.0;
@@ -92,7 +170,7 @@ struct MethodsShadowEBR : MethodCollection
 
         p_fragmentMethod =
             dynasma::makeStandalone<Method<ShaderTask>>(Method<ShaderTask>::MethodParams{
-                .tasks = {p_shadowLightFactor}, .friendlyName = "ShadowEBR"});
+                .tasks = {p_viewNormal2D, p_shadowLightFactor}, .friendlyName = "ShadowEBR"});
 
         /*
         COMPOSING
@@ -100,7 +178,7 @@ struct MethodsShadowEBR : MethodCollection
 
         auto p_shadowClear = root.getComponent<ComposeClearRenderKeeper>().new_asset(
             {ComposeClearRender::SetupParams{.root = root,
-                                             .backgroundColor = glm::vec4(0.0f, 0.5f, 0.8f, 1.0f),
+                                             .backgroundColor = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f),
                                              .displayOutputPropertyName = "shadow_cleared"}});
 
         auto p_shadowRender = root.getComponent<ComposeSceneRenderKeeper>().new_asset(
@@ -110,19 +188,27 @@ struct MethodsShadowEBR : MethodCollection
                 .vertexPositionOutputPropertyName = "position_shadow_view",
                 .displayInputPropertyName = "shadow_cleared",
                 .displayOutputPropertyName = "rendered_shadow",
-                .cullingMode = ComposeSceneRender::CullingMode::Frontface}});
-
-        auto p_shadowTexture = dynasma::makeStandalone<ComposeFrameToTexture>(
-            ComposeFrameToTexture::SetupParams{.root = root,
-                                               .frameInputPropertyName = "rendered_shadow",
-                                               .colorTextureOutputPropertyName = "",
-                                               .depthTextureOutputPropertyName = "tex_shadow",
-                                               .size = StringId("ShadowMapSize"),
-                                               .horWrap = Texture::WrappingType::BORDER_COLOR,
-                                               .verWrap = Texture::WrappingType::BORDER_COLOR,
-                                               .minFilter = Texture::FilterType::NEAREST,
-                                               .magFilter = Texture::FilterType::NEAREST,
-                                               .useMipMaps = false});
+                .cullingMode = ComposeSceneRender::CullingMode::Frontface,
+                .rasterizingMode = ComposeSceneRender::RasterizingMode::DerivationalFillCenters,
+            }});
+        auto p_shadowTexture =
+            dynasma::makeStandalone<ComposeFrameToTexture>(ComposeFrameToTexture::SetupParams{
+                .root = root,
+                .frameInputPropertyName = "rendered_shadow",
+                .depthTextureOutputPropertyName = "tex_shadow",
+                .outputTexturePropertySpecs =
+                    {
+                        ComposeFrameToTexture::OutputTexturePropertySpec{
+                            "tex_shadow_normal",
+                            PropertySpec{"normal_view2d", Variant::getTypeInfo<glm::vec2>()}},
+                    },
+                .size = StringId("ShadowMapSize"),
+                .channelType = Texture::ChannelType::VEC2_SNORM8,
+                .horWrap = Texture::WrappingType::BORDER_COLOR,
+                .verWrap = Texture::WrappingType::BORDER_COLOR,
+                .minFilter = Texture::FilterType::LINEAR,
+                .magFilter = Texture::FilterType::LINEAR,
+                .useMipMaps = false});
 
         // compose method
         p_composeMethod =
