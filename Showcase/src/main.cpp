6810f828ede6975681b8edae110b75dd531f5615
LMauricius <mauriciosmit100@gmail.com>
2024-08-23 22:25:50 +0200

    Better structure

--- Showcase/src/main.cpp
+++ Showcase/src/main.cpp
@@ -30,64 +30,67 @@ int main(int argc, char **argv)
     ComponentRoot root;
     Renderer *p_rend = new OpenGLRenderer();
     root.setComponent<Renderer>(p_rend);
-    p_rend->mainThreadSetup(root);
+    {
+        p_rend->mainThreadSetup(root);
 
-    /*
-    Assets
-    */
-    AssetCollection collection(root, *p_rend, path, sceneScale);
-    Status status;
+        /*
+        Assets
+        */
+        AssetCollection collection(root, *p_rend, path, sceneScale);
+        Status status;
 
-    /*
-    GUI setup
-    */
-    QApplication app(argc, argv);
-    SettingsWindow settingsWindow(collection, status);
-    settingsWindow.show();
-    ProfilerWindow profilerWindow(collection, status);
-    profilerWindow.show();
+        /*
+        GUI setup
+        */
+        QApplication app(argc, argv);
+        SettingsWindow settingsWindow(collection, status);
+        settingsWindow.show();
+        ProfilerWindow profilerWindow(collection, status);
+        profilerWindow.show();
 
-    /*
-    Render loop!
-    */
-    p_rend->anyThreadDisable();
-    std::thread renderThread([&]() {
-        p_rend->anyThreadEnable();
-        while (collection.running) {
-            {
-                std::unique_lock lock1(collection.accessMutex);
-
-                auto startTime = std::chrono::high_resolution_clock::now();
-                {
-                    MMETER_SCOPE_PROFILER("Render iteration");
-
-                    collection.render();
-                }
-                auto endTime = std::chrono::high_resolution_clock::now();
-
-                status.update(endTime - startTime);
-            }
-            std::this_thread::sleep_for(std::chrono::microseconds(1));
-        }
+        /*
+        Render loop!
+        */
         p_rend->anyThreadDisable();
-    });
+        std::thread renderThread([&]() {
+            p_rend->anyThreadEnable();
+            while (collection.running) {
+                {
+                    std::unique_lock lock1(collection.accessMutex);
 
-    /*
-    GUI loop!
-    */
-    while (collection.running) {
-        app.processEvents();
-        p_rend->mainThreadUpdate();
-        settingsWindow.updateValues();
-        profilerWindow.updateValues();
+                    auto startTime = std::chrono::high_resolution_clock::now();
+                    {
+                        MMETER_SCOPE_PROFILER("Render iteration");
+
+                        collection.render();
+                    }
+                    auto endTime = std::chrono::high_resolution_clock::now();
+
+                    status.update(endTime - startTime);
+                }
+                std::this_thread::sleep_for(std::chrono::microseconds(1));
+            }
+            p_rend->anyThreadDisable();
+        });
+
+        /*
+        GUI loop!
+        */
+        while (collection.running) {
+            app.processEvents();
+            p_rend->mainThreadUpdate();
+            settingsWindow.updateValues();
+            profilerWindow.updateValues();
+        }
+
+        renderThread.join();
+        p_rend->anyThreadEnable();
+
+        /*
+        Free resources
+        */
+        p_rend->mainThreadFree();
     }
-
-    renderThread.join();
-
-    /*
-    Free resources
-    */
-    p_rend->mainThreadFree();
 
     return 0;
 }
