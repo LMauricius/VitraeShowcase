f287894554057bf1d10d572b0f6c4067d1dae982
LMauricius <mauriciosmit100@gmail.com>
2024-08-31 22:14:49 +0200

    Basic Probe structs

---
+++ Showcase/src/Methods/GI/Probe.cpp
@@ -0,0 +1,59 @@
+#include "Probe.hpp"
+
+#include "MMeter.h"
+
+void GI::convertHost2GpuBuffers(std::span<const H_ProbeDefinition> hostProbes,
+                                ProbeBufferPtr gpuProbes,
+                                ReflectionBufferPtr gpuReflectionTransfers,
+                                NeighborIndexBufferPtr gpuNeighborIndices,
+                                NeighborTransferBufferPtr gpuNeighborTransfers)
+{
+    MMETER_FUNC_PROFILER;
+
+    gpuProbes.resizeElements(hostProbes.size());
+    gpuReflectionTransfers.resizeElements(hostProbes.size());
+
+    std::size_t numNeighborSpecs;
+
+    for (std::size_t i = 0; i < hostProbes.size(); i++) {
+        auto &hostProbe = hostProbes[i];
+        auto &gpuProbe = gpuProbes.getElement(i);
+        auto &gpuReflectionTransfer = gpuReflectionTransfers.getElement(i);
+
+        gpuProbe.position = glm::vec4(hostProbe.position, 0.0);
+        gpuProbe.size = glm::vec4(hostProbe.size, 0.0);
+        gpuProbe.neighborSpecBufStart = numNeighborSpecs;
+        gpuProbe.neighborSpecCount = hostProbe.neighborSpecs.size();
+
+        for (std::size_t d = 0; d < 6; d++) {
+            gpuReflectionTransfer.face[d] = glm::vec4(hostProbe.reflectionTransfer.face[d], 0.0);
+        }
+
+        numNeighborSpecs += hostProbe.neighborSpecs.size();
+    }
+
+    gpuNeighborIndices.resizeElements(numNeighborSpecs);
+    gpuNeighborTransfers.resizeElements(numNeighborSpecs);
+
+    for (std::size_t i = 0; i < hostProbes.size(); i++) {
+        auto &hostProbe = hostProbes[i];
+        auto &gpuProbe = gpuProbes.getElement(i);
+
+        for (std::size_t j = 0; j < hostProbe.neighborSpecs.size(); j++) {
+            auto &hostNeighborSpec = hostProbe.neighborSpecs[j];
+
+            gpuNeighborIndices.getElement(gpuProbe.neighborSpecBufStart + j) =
+                hostNeighborSpec.index;
+
+            auto &gpuNeighborTransfer =
+                gpuNeighborTransfers.getElement(gpuProbe.neighborSpecBufStart + j);
+
+            for (std::size_t d1 = 0; d1 < 6; d1++) {
+                for (std::size_t d2 = 0; d2 < 6; d2++) {
+                    gpuNeighborTransfer.source[d1].face[d2] =
+                        glm::vec4(hostNeighborSpec.transfer.source[d1].face[d2], 0.0);
+                }
+            }
+        }
+    }
+}
---
+++ Showcase/src/Methods/GI/Probe.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "Vitrae/Assets/SharedBuffer.hpp"
+#include "Vitrae/Types/Typedefs.hpp"
+#include "glm/glm.hpp"
+
+#include <span>
+#include <vector>
+
+using namespace Vitrae;
+
+namespace GI
+{
+
+inline constexpr glm::vec3 DIRECTIONS[] = {
+    {1.0, 0.0, 0.0},  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0},
+    {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0},  {0.0, 0.0, -1.0},
+};
+
+/*
+Cpu structs
+*/
+
+using H_Transfer = glm::vec3; // per color
+struct H_Source2FacesTransfer
+{
+    H_Transfer face[6]; // ind = to which face
+};
+struct H_NeighborTransfer
+{
+    H_Source2FacesTransfer source[6]; // ind = which neigh source
+};
+
+struct H_NeighborSpec
+{
+    std::uint32_t index;
+    H_NeighborTransfer transfer;
+};
+
+struct H_ProbeDefinition
+{
+    glm::vec3 position;
+    glm::vec3 size;
+    H_Source2FacesTransfer reflectionTransfer;
+    std::vector<H_NeighborSpec> neighborSpecs;
+};
+
+struct H_ProbeState
+{
+    glm::vec3 illumination[6];
+};
+
+/*
+Gpu structs
+*/
+
+using G_Transfer = glm::vec4; // per color
+struct G_Source2FacesTransfer
+{
+    G_Transfer face[6]; // ind = to which face
+};
+struct G_NeighborTransfer
+{
+    G_Source2FacesTransfer source[6]; // ind = which neigh source
+};
+
+struct G_ProbeDefinition
+{
+    glm::vec4 position;
+    glm::vec4 size;
+    std::uint32_t neighborSpecBufStart;
+    std::uint32_t neighborSpecCount;
+};
+
+using ProbeBufferPtr = SharedBufferPtr<void, G_ProbeDefinition>;
+using ReflectionBufferPtr = SharedBufferPtr<void, G_Source2FacesTransfer>;
+using NeighborIndexBufferPtr = SharedBufferPtr<void, std::uint32_t>;
+using NeighborTransferBufferPtr = SharedBufferPtr<void, G_NeighborTransfer>;
+
+inline constexpr const char *GLSL_DEF_SNIPPET = R"(
+const vec3 DIRECTIONS[6] = vec3[](
+    vec3(1.0, 0.0, 0.0),  vec3(-1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0),
+    vec3(0.0, -1.0, 0.0), vec3(0.0, 0.0, 1.0),  vec3(0.0, 0.0, -1.0)
+);
+
+struct Transfer {
+    vec4 color;
+};
+struct Source2FacesTransfer {
+    Transfer face[6];
+};
+struct NeighborTransfer {
+    Source2FacesTransfer source[6];
+};
+
+struct ProbeDefinition {
+    vec4 position;
+    vec4 size;
+    uint neighborSpecBufStart;
+    uint neighborSpecCount;
+};
+
+)";
+
+/*
+Conversion
+*/
+
+void convertHost2GpuBuffers(std::span<const H_ProbeDefinition> hostProbes, ProbeBufferPtr gpuProbes,
+                            ReflectionBufferPtr gpuReflectionTransfers,
+                            NeighborIndexBufferPtr gpuNeighborIndices,
+                            NeighborTransferBufferPtr gpuNeighborTransfers);
+
+} // namespace GI
