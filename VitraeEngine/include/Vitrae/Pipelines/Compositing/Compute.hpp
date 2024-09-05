a65b86f2b0d5a98559610d0d16a494803ba49194
LMauricius <mauriciosmit100@gmail.com>
2024-09-05 09:51:03 +0200

    Support for Compute shaders

---
+++ VitraeEngine/include/Vitrae/Pipelines/Compositing/Compute.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Vitrae/Pipelines/Compositing/Task.hpp"
+#include "Vitrae/Util/GpuCompute.hpp"
+
+#include "dynasma/keepers/abstract.hpp"
+
+#include <functional>
+#include <vector>
+
+namespace Vitrae
+{
+
+class ComposeCompute : public ComposeTask
+{
+  public:
+    struct SetupParams
+    {
+        ComponentRoot &root;
+        std::vector<PropertySpec> outputSpecs;
+        GpuComputeSetupParams computeSetup;
+    };
+
+    using ComposeTask::ComposeTask;
+};
+
+struct ComposeComputeKeeperSeed
+{
+    using Asset = ComposeCompute;
+    std::variant<ComposeCompute::SetupParams> kernel;
+    inline std::size_t load_cost() const { return 1; }
+};
+
+using ComposeComputeKeeper = dynasma::AbstractKeeper<ComposeComputeKeeperSeed>;
+} // namespace Vitrae
---
+++ VitraeEngine/include/Vitrae/Renderers/OpenGL/Compositing/Compute.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Vitrae/Pipelines/Compositing/Compute.hpp"
+#include "Vitrae/Util/ScopedDict.hpp"
+
+#include <functional>
+#include <vector>
+
+namespace Vitrae
+{
+
+class OpenGLRenderer;
+
+class OpenGLComposeCompute : public ComposeCompute
+{
+  public:
+    using ComposeCompute::ComposeCompute;
+
+    OpenGLComposeCompute(const SetupParams &params);
+
+    const StableMap<StringId, PropertySpec> &getInputSpecs(
+        const RenderSetupContext &args) const override;
+    const StableMap<StringId, PropertySpec> &getOutputSpecs() const override;
+
+    void run(RenderRunContext args) const override;
+    void prepareRequiredLocalAssets(StableMap<StringId, dynasma::FirmPtr<FrameStore>> &frameStores,
+                                    StableMap<StringId, dynasma::FirmPtr<Texture>> &textures,
+                                    const ScopedDict &properties) const override;
+
+    StringView getFriendlyName() const override;
+
+  protected:
+    ComponentRoot &m_root;
+    GpuComputeSetupParams m_computeSetup;
+    String m_friendlyName;
+
+    dynasma::FirmPtr<const PropertyList> mp_outputComponents;
+};
+
+} // namespace Vitrae
