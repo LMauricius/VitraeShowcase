02b1392f453f7de325f3f552fb3e29042b90fa0f
LMauricius <mauriciosmit100@gmail.com>
2024-09-05 11:19:23 +0200

    Bounding box utilities

--- VitraeEngine/include/Vitrae/Types/BoundingBox.hpp
+++ VitraeEngine/include/Vitrae/Types/BoundingBox.hpp
@@ -14,6 +14,18 @@ struct BoundingBox
     glm::vec3 getExtent() const;
 
     void merge(const BoundingBox &other);
+    void transformLeft(const glm::mat3 &transform);
+    void transformLeft(const glm::mat4 &transform);
+    void expand(float scale);
+    void expand(const glm::vec3 &scale);
+
+    void extractVertices(glm::vec3 verts[8]) const;
 };
+
+BoundingBox merged(const BoundingBox &a, const BoundingBox &b);
+BoundingBox transformed(const glm::mat3 &transform, const BoundingBox &a);
+BoundingBox transformed(const glm::mat4 &transform, const BoundingBox &a);
+BoundingBox expanded(const BoundingBox &a, float scale);
+BoundingBox expanded(const BoundingBox &a, const glm::vec3 &scale);
 
 } // namespace Vitrae
